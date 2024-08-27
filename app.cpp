8dcf7729f8ab88e17d17728489dd1f581a8634b6
CorpoWolf <gmangavin1@gmail.com>
2024-08-26 23:12:01 -0700

    plane 2 added

--- app.cpp
+++ app.cpp
@@ -24,19 +24,28 @@ MStatus PotatoPlaneCommand::doIt(const MArgList& args) {
 	MStatus status;
 
 	MFloatPointArray points;
-	points.append(MFloatPoint(-30.48f, 0.0f, -30.48f)); // bottom left
-    points.append(MFloatPoint(-30.48f, 0.0f,  30.48f)); // top left
-    points.append(MFloatPoint( 30.48f, 0.0f,  30.48f)); // top right
-    points.append(MFloatPoint( 30.48f, 0.0f, -30.48f)); // bottom right
+	points.append(MFloatPoint(-30.48f, 0.0f, -30.48f)); // back left
+	points.append(MFloatPoint(-30.48f, 0.0f,  30.48f)); // front left
+	points.append(MFloatPoint( 30.48f, 0.0f,  30.48f)); // front right
+	points.append(MFloatPoint( 30.48f, 0.0f, -30.48f)); // back right
+
+	points.append(MFloatPoint(-30.48f, 60.96f, -30.48f)); // above back left
+	points.append(MFloatPoint(30.48f, 60.96f, -30.48f));  // above back right
 
 	MIntArray faceCounts;
-	faceCounts.append(4);
+	faceCounts.append(4); //floor
+	faceCounts.append(4); //back wall
 
 	MIntArray faceConnects;
 	faceConnects.append(0);
 	faceConnects.append(1);
 	faceConnects.append(2);
 	faceConnects.append(3);
+
+	faceConnects.append(4); // above back left
+	faceConnects.append(0); // back left
+	faceConnects.append(3); // back right
+	faceConnects.append(5); // above back right
 
 	MFnMesh meshFn;
 	MObject mesh = meshFn.create(points.length(), faceCounts.length(), points, faceCounts, faceConnects, MObject::kNullObj, &status);
@@ -49,21 +58,21 @@ MStatus PotatoPlaneCommand::doIt(const MArgList& args) {
 	dagNode.setName("PotatoPlane");
 
 	MSelectionList selectionList;
-    selectionList.add("initialShadingGroup");
-    MObject shadingGroupNode;
-    selectionList.getDependNode(0, shadingGroupNode);
+	selectionList.add("initialShadingGroup");
+	MObject shadingGroupNode;
+	selectionList.getDependNode(0, shadingGroupNode);
 
-    if (shadingGroupNode.isNull()) {
-        MGlobal::displayError("initialShadingGroup shading group not found. Make sure it exists in the scene.");
-        return MStatus::kFailure;
-    }
+	if (shadingGroupNode.isNull()) {
+		MGlobal::displayError("initialShadingGroup shading group not found. Make sure it exists in the scene.");
+		return MStatus::kFailure;
+	}
 
 	MFnSet shadingGroup(shadingGroupNode);
-    shadingGroup.addMember(mesh);
+	shadingGroup.addMember(mesh);
 
-    MGlobal::displayInfo("PotatoPlane created successfully with initialShadingGroup Shading Group assigned!");
+	MGlobal::displayInfo("PotatoPlane created successfully with initialShadingGroup Shading Group assigned!");
 
-    return MS::kSuccess;
+	return MS::kSuccess;
 }
 
 MStatus initializePlugin(MObject obj) {

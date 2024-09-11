3c60935ec46fb349d61160306149eec15c865b05
CorpoWolf <gmangavin1@gmail.com>
2024-09-11 11:15:10 -0700

    template util test

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,17 +6,19 @@
 #include <maya/MStatus.h>
 #include <maya/MGlobal.h>
 
-// MSelectionList can have mutliple various data types
-void archCmdFlag(MArgDatabase& argData, const char* flag, MString& arg) {
+template <typename T>
+void cmdFlag(MArgDatabase& argData, const char* flag, T& arg) {
 	if (argData.isFlagSet(flag)) {
 		argData.getFlagArgument(flag, 0, arg);
 		MGlobal::displayInfo(MString("Flag ") + flag + " is set.");
 	}
 }
 
+// MSelectionList can have mutliple various data types
+void archCmdFlag(MArgDatabase& argData, const char* flag, MString& arg) {
+	cmdFlag(argData, flag, arg);
+}
+
 void archCmdFlag(MArgDatabase& argData, const char* flag, uint32_t& arg) {
-	if (argData.isFlagSet(flag)) {
-		argData.getFlagArgument(flag, 0, arg);
-		MGlobal::displayInfo(MString("Flag ") + flag + " is set.");
-	}
+	cmdFlag(argData, flag, arg);
 }

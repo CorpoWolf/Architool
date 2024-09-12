002b2233c4add6c21650108a17ca0c33ef8a0bcb
CorpoWolf <gmangavin1@gmail.com>
2024-09-12 16:11:14 -0700

    namespace for util

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace archiUtil {
-	void archiCmdFlag(MArgDatabase& argData, const char* flag, MString& arg, MPlug& plug);
-	void archiCmdFlag(MArgDatabase& argData, const char* flag, uint32_t& arg, MPlug& plug);
+	void CmdFlag(MArgDatabase& argData, const char* flag, MString& arg, MPlug& plug);
+	void CmdFlag(MArgDatabase& argData, const char* flag, uint32_t& arg, MPlug& plug);
 }
